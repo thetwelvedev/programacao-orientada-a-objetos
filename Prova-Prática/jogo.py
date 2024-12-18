b595554c182ffa6ff78bf5d19b6b62b4ce98b2e9
Leonardo Castro <twelve.programming@gmail.com>
2024-12-18 13:11:48 -0400

    Função interagir_com_personagem finalizada

--- Prova-Prática/jogo.py
+++ Prova-Prática/jogo.py
@@ -62,7 +62,7 @@ def alimentar_personagem(personagens):
             else: 
                 print("Não tem mocinhos!")
         case "2": #Quando dor vilão
-            viloes = [p for p in personagens if isinstance(p, Vilao)] #Cria uma lista só com os Mocinhos
+            viloes = [p for p in personagens if isinstance(p, Vilao)] #Cria uma lista só com os Viloes
             if viloes: #Se a lista tiver vazia vai ser False e não vai executar
                 print("\n>>>>>>>>>> Escolha o Vilão <<<<<<<<<<")
                 for i, vilao in enumerate(viloes): #Vai listar os Vilões
@@ -76,7 +76,55 @@ def alimentar_personagem(personagens):
             return
 
 def interagir_com_personagem(personagens):
-    pass
+    print("""\n>>>>>>>>>> Escolha o Personagem para Interagir <<<<<<<<<<
+1. Mocinho
+2. Vilão
+3. Voltar
+""")
+    opcao = input("Escolha uma opção: ")
+
+    match opcao:
+        case "1": #Quando for mocinho
+            mocinhos = [p for p in personagens if isinstance(p, Mocinho)] #Cria uma lista só com os Mocinhos
+            if mocinhos: #Se a lista tiver vazia vai ser False e não vai executar
+                print("\n>>>>>>>>>> Escolha o Mocinho <<<<<<<<<<")
+                for i, mocinho in enumerate(mocinhos): #Vai listar os Mocinhos
+                        print(f"{i+1}. {mocinho.nome}")
+                escolha = int(input("Escolha o número do mocinho: "))
+                personagem_selecionado = mocinhos[escolha - 1] #Pois o índice começa em zero
+                #Escolhendo o personagem para interagir
+                print("\n>>>>>>>>>> Escolha o Personagem <<<<<<<<<<")
+                personagens_excluindo_selecionado = [p for p in personagens if p != personagem_selecionado] #Assim seleciona um personagem sem incluir o personagem selecioando
+                for i, p in enumerate(personagens_excluindo_selecionado):
+                    print(f"{i+1}. {p.nome}")
+                interacao_escolha = int(input("Escolha o número do personagem: "))
+                outro_personagem = personagens_excluindo_selecionado[interacao_escolha - 1]
+                personagem_selecionado.interagir(outro_personagem)  #Aqui tem interação entre um morcinho e um personagem qualquer
+            else:
+                print("Não tem mocinhos!")
+        
+        case "2": #Quando dor vilão
+            viloes = [p for p in personagens if isinstance(p, Vilao)] #Cria uma lista só com os Viloes
+            if viloes: #Se a lista tiver vazia vai ser False e não vai executar
+                print("\n>>>>>>>>>> Escolha o Vilão <<<<<<<<<<")
+                for i, vilao in enumerate(viloes): #Vai listar os Vilões
+                        print(f"{i+1}. {vilao.nome}")
+                escolha = int(input("Escolha o número do mocinho: "))
+                personagem_selecionado = viloes[escolha - 1] #Pois o índice começa em zero
+                #Escolhendo o personagem para interagir
+                print("\n>>>>>>>>>> Escolha o Personagem <<<<<<<<<<")
+                personagens_excluindo_selecionado = [p for p in personagens if p != personagem_selecionado] #Assim seleciona um personagem sem incluir o personagem selecioando
+                for i, p in enumerate(personagens_excluindo_selecionado):
+                    print(f"{i+1}. {p.nome}")
+                interacao_escolha = int(input("Escolha o número do personagem: "))
+                outro_personagem = personagens_excluindo_selecionado[interacao_escolha - 1]
+                personagem_selecionado.interagir(outro_personagem)  #Aqui tem interação entre um vilao e um personagem qualquer
+            else:
+                print("Não tem vilões!")
+        
+        case "3":
+            return
+
 
 def menu():
     personagens = []  #Vai armazenar todos os personagens
